c5c4eb8076e08d5b2f67d65e610b865f877f0703
mount986 <matthew.curtice@gmail.com>
2023-12-11 09:00:13 -0500

    Day05

--- advent/day05/day05.go
+++ advent/day05/day05.go
@@ -1,14 +1,21 @@
 package day05
 
-import "fmt"
+import (
+	"slices"
+)
 
 type Day05 struct {
+	garden Garden
 }
 
 func (a *Day05) Part1(input []string) (any, error) {
-	return nil, fmt.Errorf("day 05 part 1 is not yet implemented")
+	a.garden.ReadMaps(input, BasicSeeds)
+
+	return slices.Min(a.garden.collections["location"]), nil
 }
 
 func (a *Day05) Part2(input []string) (any, error) {
-	return nil, fmt.Errorf("day 05 part 2 is not yet implemented")
+	a.garden.ReadMaps(input, ComplexSeeds)
+
+	return slices.Min(a.garden.collections["location"]), nil
 }
--- advent/day05/day05_test.go
+++ advent/day05/day05_test.go
@@ -23,7 +23,7 @@ func TestPart1(t *testing.T) {
 		input = append(input, scanner.Text())
 	}
 
-	expected := 142
+	expected := 35
 	day05 := Day05{}
 	got, err := day05.Part1(input)
 	assert.NoError(t, err)
@@ -31,5 +31,24 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
+	var (
+		file    *os.File
+		input   []string
+		scanner *bufio.Scanner
+		err     error
+	)
+	file, err = os.Open("test.dat")
+	if err != nil {
+		t.Fatal("Failed to open test data file")
+	}
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
 
+	expected := 46
+	day05 := Day05{}
+	got, err := day05.Part2(input)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, got.(int))
 }
